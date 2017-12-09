1c2a0c6764abbbc687aea59bb5665c6b00f16971
radha226 <radhasingh226@gmail.com>
2017-12-09 17:37:49 +0530

    changes

---
+++ src/pages/contact-us/contact-us.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the ContactUsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-contact-us',
+  templateUrl: 'contact-us.html',
+
+})
+export class ContactUsPage {
+	modaldata:any;
+  constructor(public navCtrl: NavController, public viewctrl:ViewController, public navParams: NavParams) {
+  		
+
+  }
+  CloseModal(){
+  	this.viewctrl.dismiss();
+  }
+  ionViewWillLoad() {
+    console.log('ionViewDidLoad ContactUsPage');
+     this.modaldata=this.navParams.get('ModelData');    
+    console.log(this.modaldata);
+
+
+
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+	import { Component } from '@angular/core';
+	import { NavController,LoadingController , ModalController } from 'ionic-angular';
+	import  {ContactUsPage} from '../contact-us/contact-us';
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
-export class HomePage {
+	@Component({
+	  templateUrl: 'home.html',
+	  selector: 'page-home',
+	})
+	export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	  constructor(public navCtrl: NavController, public loadingctrl:LoadingController , private modalctrl:ModalController) {
 
-  }
+	  }
+	  loader(){
+	  	let laoder=this.loadingctrl.create({
+	  		content:'loading......'
+	  	})
+	  	laoder.present();
+	  }
+	  modelopen(){
+	  	let ModelData={name:'contactpage'};
+	  	let model=this.modalctrl.create(ContactUsPage , {ModelData});
+	  	model.present();
+	  }
 
-}
+	}
